c221b7d3aba034eb0234ce845a67c063876b2ea1
linin <lianjie35@outlook.com>
2022-12-12 22:47:06 +0800

    add: body controller

--- Gesture/core/type/Gesture.py
+++ Gesture/core/type/Gesture.py
@@ -1,18 +1,20 @@
 '''
 Author: linin00
 Date: 2022-12-07 18:37:01
-LastEditTime: 2022-12-12 15:28:42
+LastEditTime: 2022-12-12 22:44:41
 LastEditors: linin00
 Description: 
 FilePath: /open/Gesture/core/type/Gesture.py
 
 '''
+from enum import Enum
 # 定义的一些动作
 class Gesture(Enum):
   HEAD = 0
-  LEFTARM = 1
-  RIGHTARM = 2
-  BODY = 3
-  LEFTLEG = 4
-  RIGHTLEG = 5
-  STANDBY = 6
+  LEFTARM = 12
+  RIGHTARM = 11
+  BODY = 1
+  LEFTLEG = 24
+  RIGHTLEG = 23
+  STANDBY = 2
+  RIGHTHAND = 19
