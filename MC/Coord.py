c4873fa85a3e5192fa46f079bcee1ece91845596
9486Phil <unconfigured@null.spigotmc.org>
2022-12-13 23:30:13 +0800

    Modify User Data

---
+++ MC/Coord.py
@@ -0,0 +1,39 @@
+from mcpi.vec3 import Vec3
+
+origin = Vec3(553,8,1373)
+
+BlueMoon06 = {
+        "coord_boots" : [20006-origin.x,6-origin.y,19992-origin.z],
+        "coord_chestplate" : [20006-origin.x,6-origin.y,19988-origin.z],
+        "coord_helmet" : [20006-origin.x,6-origin.y,19986-origin.z],
+        "coord_ln" : [20006-origin.x,6-origin.y,19984-origin.z],
+        "coord_tnt" : [20006-origin.x,6-origin.y,19982-origin.z],
+        "coord_cage" : [20006-origin.x,6-origin.y,19976-origin.z] 
+}
+
+User2 = {
+        "coord_boots" : [20006-origin.x,6-origin.y,19992-origin.z],
+        "coord_chestplate" : [20006-origin.x,6-origin.y,19988-origin.z],
+        "coord_helmet" : [20006-origin.x,6-origin.y,19986-origin.z],
+        "coord_ln" : [0,0,0],
+        "coord_tnt" : [0,0,0],
+        "coord_cage" : [0,0,0]  
+}
+
+User3 = {
+        "coord_boots" : [20006-origin.x,6-origin.y,19992-origin.z],
+        "coord_chestplate" : [20006-origin.x,6-origin.y,19988-origin.z],
+        "coord_helmet" : [20006-origin.x,6-origin.y,19986-origin.z],
+        "coord_ln" : [0,0,0],
+        "coord_tnt" : [0,0,0],
+        "coord_cage" : [0,0,0]  
+}
+
+User4 = {
+        "coord_boots" : [20006-origin.x,6-origin.y,19992-origin.z],
+        "coord_chestplate" : [20006-origin.x,6-origin.y,19988-origin.z],
+        "coord_helmet" : [20006-origin.x,6-origin.y,19986-origin.z],
+        "coord_ln" : [0,0,0],
+        "coord_tnt" : [0,0,0],
+        "coord_cage" : [0,0,0]  
+}
--- MC/function_MC.py
+++ MC/function_MC.py
@@ -1,27 +1,24 @@
 import sys
 sys.path.append("MC")
 from MC_utils import *
+from Coord import *
 sys.path.append("Gesture/type")
 import Msg 
 
 origin = Vec3(553,8,1373)
 
-BlueMoon06 = {
-        "coord_boots" : [20006-origin.x,6-origin.y,19992-origin.z],
-        "coord_chestplate" : [20006-origin.x,6-origin.y,19988-origin.z],
-        "coord_helmet" : [20006-origin.x,6-origin.y,19986-origin.z],
-        "coord_ln" : [0,0,0],
-        "coord_tnt" : [0,0,0],
-        "coord_cage" : [0,0,0]   
-    }
-
 players = {
-    "BlueMoon06":BlueMoon06
-    
+    "BlueMoon06":BlueMoon06,
+    "User2":User2,
+    "User3":User3,
+    "User4":User4    
 }
 
 ready = {
     "BlueMoon06":[False,False,False],
+    "User2":[False,False,False],
+    "User3":[False,False,False],
+    "User4":[False,False,False]
 }
 
 def my_handleWearing(_players: list, msg: str) -> bool:
